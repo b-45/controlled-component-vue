2f773280bc9309c365f00930df58c4bdf9d3500e
Bernard Ade <bernard@b-45.com>
2018-07-12 21:38:59 -0400

    created ToggleInput compoenent, modified css and App.vue

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+
+  <div class="min-h-screen bg-grey-light p-8">
+    <div class="max-w-sm mx-auto">
+      <user-sign-up-form></user-sign-up-form>
+    </div>
+  </div>
+</template>
+
+<script>
+import UserSignUpForm from "./components/UserSignUpForm.vue";
+
+export default {
+  name: "app",
+  components: {
+    UserSignUpForm
+  }
+};
+</script>
+
+<style>
+body {
+  font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Oxygen,
+    Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+}
+</style>
